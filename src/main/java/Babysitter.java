42d4dacb8da6f821307c6575e9c88e99dac4bbb8
BrookeHau <brookerhauser@gmail.com>
2018-03-25 20:45:38 -0400

    calculated pay before bedtime

--- src/main/java/Babysitter.java
+++ src/main/java/Babysitter.java
@@ -1,8 +1,28 @@
 
 public class Babysitter {
 
-	public int getPay() {
-		return 0;
+	private int startTime;
+	private int bedTime;
+	private int endTime;
+	private int payRate1 = 12;
+	private int payRate2 = 8;
+	private int payRate3 = 16;
+
+	public Babysitter(int startTime, int bedTime, int endTime) {
+		this.startTime = startTime;
+		this.bedTime = bedTime;
+		this.endTime = endTime;
 	}
 
+	public int getPay() {
+		return payBeforeBedTime();
+	}
+
+	public int payBeforeBedTime (){
+		return hoursBeforeBedTime() * payRate1;
+	}
+	
+	public int hoursBeforeBedTime() {
+		return bedTime - startTime;
+	}
 }
--- src/test/java/BabysitterTest.java
+++ src/test/java/BabysitterTest.java
@@ -7,9 +7,16 @@ public class BabysitterTest {
 
 	@Test
 	public void shouldGetPay() {
-		Babysitter babysitter = new Babysitter();
+		Babysitter babysitter = new Babysitter(0, 0, 0);
 		int pay = babysitter.getPay();
 		assertThat(pay, is(0));
 	}
 
+	@Test
+	public void calculatePayTilBedTime() {
+		Babysitter babysitter = new Babysitter(17, 19, 4);
+		int pay = babysitter.getPay();
+		assertThat(pay, is(24));
+		
+	}
 }
